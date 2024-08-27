519d9ddda651c78a7afb45827d81cedb05d05319
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2024-08-27 14:59:41 +0530

    changes

--- WearOSTest/app/src/main/java/com/example/wearostest/presentation/MainActivity.kt
+++ WearOSTest/app/src/main/java/com/example/wearostest/presentation/MainActivity.kt
@@ -9,18 +9,25 @@ package com.example.wearostest.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
@@ -36,23 +43,33 @@ class MainActivity : ComponentActivity() {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
-            WearApp("Android")
+            WearOSTestTheme {
+                WearApp()
+            }
         }
     }
 }
 
 @Composable
-fun WearApp(greetingName: String) {
-    WearOSTestTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            contentAlignment = Alignment.Center
-        ) {
-            TimeText()
-            Greeting(greetingName = greetingName)
-        }
+fun WearApp() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        contentAlignment = Alignment.Center
+    ) {
+        TimeText()
+        Button(
+            onClick = { TODO() },
+            content = {
+                Box(
+                    modifier = Modifier
+                        .size(16.dp)
+                        .clip(shape = RoundedCornerShape(corner = CornerSize(size = 4.dp)))
+                        .background(color = Color.Black)
+                )
+            }
+        )
     }
 }
 
@@ -69,5 +86,5 @@ fun Greeting(greetingName: String) {
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    WearApp()
 }
