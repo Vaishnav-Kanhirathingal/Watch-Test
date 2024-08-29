d598676ffe807556ad25429b10cd058a12c63b90
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2024-08-29 20:58:06 +0530

    watch app partially functional

--- WearOSTest/app/src/main/java/com/example/wearostest/presentation/MainActivity.kt
+++ WearOSTest/app/src/main/java/com/example/wearostest/presentation/MainActivity.kt
@@ -6,137 +6,38 @@
 
 package com.example.wearostest.presentation
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.wear.compose.material.Button
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TimeText
 import com.example.wearostest.presentation.theme.WearOSTestTheme
-import com.google.android.gms.wearable.PutDataRequest
-import com.google.android.gms.wearable.Wearable
+import com.example.wearostest.service.CounterService
+import com.example.wearostest.ui.HomeScreen
 
 class MainActivity : ComponentActivity() {
+    private lateinit var counterService: CounterService
+    private lateinit var message: MutableState<String?>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
-
         super.onCreate(savedInstanceState)
-
         setTheme(android.R.style.Theme_DeviceDefault)
-
+        counterService = CounterService(
+            context = this,
+            setMessage = { message.value = it }
+        )
         setContent {
+            message = remember { mutableStateOf<String?>(null) }
             WearOSTestTheme {
-                WearApp()
+                HomeScreen.WearApp(
+                    message = message.value,
+                    counterService = counterService
+                )
             }
         }
+        counterService.startListener()
     }
 }
-
-@Composable
-fun WearApp() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background),
-        contentAlignment = Alignment.Center
-    ) {
-        TimeText()
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-            content = {
-                val context = LocalContext.current
-                Button(
-                    onClick = { test(context = context) },
-                    content = {
-                        Box(
-                            modifier = Modifier
-                                .size(16.dp)
-                                .clip(shape = RoundedCornerShape(corner = CornerSize(size = 4.dp)))
-                                .background(color = Color.Black)
-                        )
-                    }
-                )
-                Text(text = "random text")
-            }
-        )
-    }
-}
-
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp()
-}
-
-private const val TAG = "MainActivity"
-
-private const val TEST_PATH = "/test/request"
-private const val NODE_ID = "node_id"
-
-fun test(context: Context) {
-    Wearable.getMessageClient(context)
-        .sendMessage(NODE_ID, TEST_PATH, "some message".toByteArray())
-        .addOnSuccessListener { Log.d(TAG, "sendMessage success") }
-        .addOnFailureListener { Log.d(TAG, "sendMessage failure");it.printStackTrace() }
-
-
-    Wearable.getDataClient(context)
-        .putDataItem(
-            PutDataRequest.create(TEST_PATH)
-                .setData(
-                    "\"~Some jumbled words~\" - Wear OS".toByteArray()
-                )
-        )
-        .addOnSuccessListener { Log.d(TAG, "putDataItem success") }
-        .addOnFailureListener { Log.d(TAG, "putDataItem failure");it.printStackTrace() }
-}
-
-//private const val VOICE_TRANSCRIPTION_CAPABILITY_NAME = "voice_transcription"
-//
-//private fun setupVoiceTranscription(context: Context) {
-//    val capabilityInfo: CapabilityInfo = Tasks.await(
-//        Wearable.getCapabilityClient(context)
-//            .getCapability(
-//                VOICE_TRANSCRIPTION_CAPABILITY_NAME,
-//                CapabilityClient.FILTER_REACHABLE
-//            )
-//    )
-//    updateTranscriptionCapability(capabilityInfo).also { capabilityListener ->
-//        Wearable.getCapabilityClient(context).addListener(
-//            capabilityListener,
-//            VOICE_TRANSCRIPTION_CAPABILITY_NAME
-//        )
-//    }
-//}
-//private var transcriptionNodeId: String? = null
-//
-//private fun updateTranscriptionCapability(capabilityInfo: CapabilityInfo) {
-//    transcriptionNodeId = pickBestNodeId(capabilityInfo.nodes)
-//}
-//
-//private fun pickBestNodeId(nodes: Set<Node>): String? {
-//    // Find a nearby node or pick one arbitrarily.
-//    return nodes.firstOrNull { it.isNearby }?.id ?: nodes.firstOrNull()?.id
-//}
---
+++ WearOSTest/app/src/main/java/com/example/wearostest/ui/HomeScreen.kt
@@ -0,0 +1,93 @@
+package com.example.wearostest.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
+import com.example.wearostest.service.CounterService
+
+object HomeScreen {
+    @Composable
+    fun WearApp(
+        message: String?,
+        counterService: CounterService
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            contentAlignment = Alignment.Center
+        ) {
+            TimeText()
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(
+                    space = 4.dp,
+                    alignment = Alignment.CenterVertically
+                ),
+                content = {
+                    val context = LocalContext.current
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 4.dp),
+                        textAlign = TextAlign.Center,
+                        text = message ?: "[MESSAGE]"
+                    )
+                    Button(
+                        onClick = {
+                            counterService.sendTrigger(
+                                context = context,
+                            )
+//                            counterService.sendTrigger(
+//                                context = context,
+//                                message = System.currentTimeMillis().toString()
+//                            )
+                        },
+                        content = {
+                            Box(
+                                modifier = Modifier
+                                    .size(16.dp)
+                                    .clip(shape = RoundedCornerShape(corner = CornerSize(size = 4.dp)))
+                                    .background(color = Color.Black)
+                            )
+                        }
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    HomeScreen.WearApp(
+        message = "",
+        counterService = CounterService(
+            context = LocalContext.current,
+            setMessage = {}
+        )
+    )
+}
