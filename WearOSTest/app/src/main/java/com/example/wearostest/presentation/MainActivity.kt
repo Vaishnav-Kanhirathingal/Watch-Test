562c090b410dd0161e622cfd60d8904bf1c6213d
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2024-08-29 15:37:52 +0530

    code added

--- WearOSTest/app/src/main/java/com/example/wearostest/presentation/MainActivity.kt
+++ WearOSTest/app/src/main/java/com/example/wearostest/presentation/MainActivity.kt
@@ -6,13 +6,16 @@
 
 package com.example.wearostest.presentation
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,8 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,8 +33,9 @@ import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
-import com.example.wearostest.R
 import com.example.wearostest.presentation.theme.WearOSTestTheme
+import com.google.android.gms.wearable.PutDataRequest
+import com.google.android.gms.wearable.Wearable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,20 +62,27 @@ fun WearApp() {
         contentAlignment = Alignment.Center
     ) {
         TimeText()
-        Button(
-            onClick = { TODO() },
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp),
             content = {
-                Box(
-                    modifier = Modifier
-                        .size(16.dp)
-                        .clip(shape = RoundedCornerShape(corner = CornerSize(size = 4.dp)))
-                        .background(color = Color.Black)
+                val context = LocalContext.current
+                Button(
+                    onClick = { test(context = context) },
+                    content = {
+                        Box(
+                            modifier = Modifier
+                                .size(16.dp)
+                                .clip(shape = RoundedCornerShape(corner = CornerSize(size = 4.dp)))
+                                .background(color = Color.Black)
+                        )
+                    }
                 )
+                Text(text = "random text")
             }
         )
     }
 }
-
 
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
@@ -80,3 +90,48 @@ fun DefaultPreview() {
     WearApp()
 }
 
+//private const val VOICE_TRANSCRIPTION_CAPABILITY_NAME = "voice_transcription"
+//
+//private fun setupVoiceTranscription(context: Context) {
+//    val capabilityInfo: CapabilityInfo = Tasks.await(
+//        Wearable.getCapabilityClient(context)
+//            .getCapability(
+//                VOICE_TRANSCRIPTION_CAPABILITY_NAME,
+//                CapabilityClient.FILTER_REACHABLE
+//            )
+//    )
+//    updateTranscriptionCapability(capabilityInfo).also { capabilityListener ->
+//        Wearable.getCapabilityClient(context).addListener(
+//            capabilityListener,
+//            VOICE_TRANSCRIPTION_CAPABILITY_NAME
+//        )
+//    }
+//}
+//private var transcriptionNodeId: String? = null
+//
+//private fun updateTranscriptionCapability(capabilityInfo: CapabilityInfo) {
+//    transcriptionNodeId = pickBestNodeId(capabilityInfo.nodes)
+//}
+//
+//private fun pickBestNodeId(nodes: Set<Node>): String? {
+//    // Find a nearby node or pick one arbitrarily.
+//    return nodes.firstOrNull { it.isNearby }?.id ?: nodes.firstOrNull()?.id
+//}
+
+private const val TEST_PATH = "/test/request"
+private const val TAG = "MainActivity"
+
+fun test(context: Context) {
+//    Wearable.getMessageClient(context)
+//        .sendMessage(TEST_PATH, TAG, "some message".toByteArray())
+
+    Wearable.getDataClient(context)
+        .putDataItem(
+            PutDataRequest.create(TEST_PATH)
+                .setData(
+                    "\"~Some jumbled words~\" - Wear OS".toByteArray()
+                )
+        )
+        .addOnSuccessListener { Log.d(TAG, "success") }
+        .addOnFailureListener { Log.d(TAG, "failure");it.printStackTrace() }
+}
